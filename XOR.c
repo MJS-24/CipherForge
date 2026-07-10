b771e31ed77141a976b26ecdd497e3dc12003a41
乃ЕТА <187012799+MJS-24@users.noreply.github.com>
2026-07-10 22:09:30 +0800

    XOR

--- XOR.c
+++ XOR.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int XOR(command, key){
-
+void XOR(char command[],int lenght, unsigned char key){
+  for(int i = 0; i < lenght; i++){
+    command[i] = command[i] ^ key;
+  }
 }
 
 int main(int argc, char *argv[]) {
   
+  int len;
   char command[200];
   unsigned char key;
 
   if (argc > 1){
 
-    key = argv[1];
+    key = (unsigned char)strtol(argv[1], NULL, 16);
     fgets(command, sizeof(command), stdin);
 
-  } else {  
-    printf("Hello world");
-  }
+    command[strcspn(command, "\n")] = '\0';
+    len = strlen(command);
 
+    XOR(command, len, key);
+
+    printf("Encrypted: ");
+
+    for(int i = 0; i < len; i++){
+      printf("0x%02x ", (unsigned char)command[i]);
+    }
+
+  } else {  
+    printf("Enter key");
+  }
 
   return 0;
 }
